a76f7099bd6d3ca293baeb76394fb18e7470b5a9
Erik Hansen <e.hansen31@live.com>
2019-07-06 07:53:21 -0700

    char arrays example now working

---
+++ chapter_1/char_arrays.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getUserLine(char s[], int lim);
+void copy(char to[], char from[]);
+
+main()
+{
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while ((len = getUserLine(line, MAXLINE) > 0))
+	{
+		printf("made it inside while loop\n");
+		if (len > max)
+		{
+			max = len;
+			copy(longest, line);
+		}
+		printf("value of max is %d\n", max);
+		if (max > 0)
+		{
+			printf("%s", longest);
+		}
+	}
+
+	return 0;
+}
+
+int getUserLine(char s[], int lim)
+{
+	printf("made it to getUserLine\n");
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+	{
+		s[i] = c;
+	}
+	if (c == '\n')
+	{
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+	{
+		++i;
+	}
+}
