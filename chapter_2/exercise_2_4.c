3b6e93b04091ed0e0ae668d280e23cb3b6edcf9f
Erik Hansen <e.hansen31@live.com>
2019-07-17 18:52:15 -0400

    start exercise 2_5

--- chapter_2/exercise_2_4.c
+++ chapter_2/exercise_2_4.c
@@ -7,12 +7,13 @@ each character in s1 that matches any character in the string s2.
 char result[1000];
 int main(int argc, char const *argv[])
 {
-    char* message = "hello world";
-    char * exclude = "aeiou";
-    squeeze2(message, exclude);
-    // squeeze("hello world", "aeiou");
-        /* code */
-        printf("string \"hello world\" excluding \"aeiou\" is: %s", message);
+    char *message = "hello world";
+    char *exclude = "aeiou";
+
+    squeeze("hello world", "aeiou");
+
+    printf("string \"hello world\" excluding \"aeiou\" is: %s\n", result);
+
     return 0;
 }
 
@@ -24,26 +25,17 @@ void squeeze(char s1[], char s2[])
     {
         for (int j = 0; s2[j] != '\0'; j++)
         {
-            if (s1[j] == s2[j])
+            if (s1[i] == s2[j])
             {
+                // printf("s1[%d]:%c is equal to s2[%d]:%c\n", i, s1[i], j, s2[j]);
                 break;
             }
             if (s2[j + 1] == '\0')
             {
+                // printf("%c appeneded to result array\n", s1[i]);
                 result[result_radix++] = s1[i];
             }
         }
     }
     return;
-}
-
-void
-squeeze2(char s[], char t[]) {
-    int i, j, k;
-    for (k = 0; t[k] != '\0'; k++) {
-        for (i = j = 0; s[i] != '\0'; i++)
-            if (s[i] != t[k])
-                s[j++] = s[i];
-        s[j] = '\0';
-    }
 }
