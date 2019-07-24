97e1c20c50114a5ce326ffdc7c038359b8bf9d1e
Erik Hansen <e.hansen31@live.com>
2019-07-23 20:36:47 -0400

    complete exercise 2_5

--- chapter_2/exercise_2_5.c
+++ chapter_2/exercise_2_5.c
@@ -3,11 +3,31 @@ Write the function any (s1, s2 ), which returns the first location
 in the string s1 where any character from the string s2 occurs, or -1 if s1 contains no characters from s2. (The standard library function strpbrk does the same job but returns a pointer to the location.
  */
 
+#include <stdio.h>
+
 int main(int argc, char const *argv[])
-{    
+{
+    char *value = "this is the input statment";
+    char *checked = "words here shouldn't be in the string above";
+
+    int result = any(value, checked);
+
+    printf("string \"%s\" which shouldn't contain \"%s\" is: %d\n", value, checked, result);
+
     return 0;
 }
 
-int any()
+int any(char s1[], char s2[])
 {
+    for (int i = 0; s1[i] != '\0'; i++)
+    {
+        for (int j = 0; s1[j] != '\0'; j++)
+        {
+            if (s1[i] == s2[j])
+            {
+                return i;
+            }
+        }
+    }
+    return (-1);
 }
